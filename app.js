41d705c3ad8e5c647a79102000a56379b1612a71
Kim Tore Kristensen <kimelim89@gmail.com>
2017-11-19 14:18:26 +0100

    bom

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+
+app.use(express.static('public'));
+
+app.get('/search', function (req, res) {
+  fs.readFile(__dirname + '/contacts.json', 'utf8', function (err, data) {
+    res.end(data);
+  });
+});
+
+app.get('/search/:navn', function (req, res) {
+  fs.readFile(__dirname + '/contacts.json', 'utf8', function (err, data) {
+    jsondata = JSON.parse(data);
+    result = [];
+    for (var i = 0; i < jsondata.length; i++) {
+      var navn = jsondata[i].displayName.toLowerCase();
+      var sokresult = req.params.navn.toLowerCase();
+      if (navn.includes(sokresult)) {
+        result.push(jsondata[i]);
+      }
+    }
+    res.end(JSON.stringify(result));
+
+  });
+
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+var server = app.listen(7600, function () {
+  var host = server.address().address
+  var port = server.address().port
+});
